ba6061bf5bf184f5a7177130213468ba5e08c289
Abraham Alkhatib <aaalkhatib46664@gmail.com>
2023-07-09 20:07:12 -0400

    More bug fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@
 /*   By: Abraham Alkhatib <aaa26@illinois.edu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 14:30:53 by Abraham Alk       #+#    #+#             */
-/*   Updated: 2023/07/09 20:03:42 by Abraham Alk      ###   ########.fr       */
+/*   Updated: 2023/07/09 20:06:55 by Abraham Alk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,39 +30,36 @@ const Projects = (props) => {
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:gap-16 md:gap-8 sm:gap-8 justify-center'>
             {projects.map((info, index) => (
                 
-                // Different link types need different link formats
-                (info.linkType == "internal") ?
-                <a href={info.link} className='hover:cursor-pointer'>
-                    <article className={`group ${asideBackground} rounded-2xl my-5`}>
-                        <img
-                            src={info.img}
-                            class="h-56 w-full transition group-hover:grayscale-[50%] p-2"
-                        />
-
-                        <div class="p-4">
-                            <h3 class={`text-lg font-medium bg-clip-text text-transparent font-Orbitron ${props.foreground.accent}`}>
-                                {info.title}
-                            </h3>
-
-                            <p class={`mt-2 line-clamp-3 text-sm/relaxed ${props.colorMode.text}`}>
-                                {info.description}
-                            </p>
-                        </div>
-                    </article>
-                </a>
-                :
+                // // Different link types need different link formats
+                // (info.linkType == "internal") ?
+                // <a href={info.link} className='hover:cursor-pointer'>
+                //     <article className={`group ${asideBackground} rounded-2xl my-5`}>
+                //         <img
+                //             src={info.img}
+                //             class="h-56 w-full transition group-hover:grayscale-[50%] p-2"
+                //         />
+                //         <div class="p-4">
+                //             <h3 class={`text-lg font-medium bg-clip-text text-transparent font-Orbitron ${props.foreground.accent}`}>
+                //                 {info.title}
+                //             </h3>
+                //             <p class={`mt-2 line-clamp-3 text-sm/relaxed ${props.colorMode.text}`}>
+                //                 {info.description}
+                //             </p>
+                //         </div>
+                //     </article>
+                // </a>
+                // :
+                
                 <Link to={`${info.link}`}>
                     <article className={`group ${asideBackground} rounded-2xl my-5 hover:cursor-pointer`}>
                         <img
                             src={info.img}
                             class="h-56 w-full transition group-hover:grayscale-[50%] p-2"
                         />
-
                         <div class="p-4">
                             <h3 class={`text-lg font-medium bg-clip-text text-transparent font-Orbitron ${props.foreground.accent}`}>
                                 {info.title}
                             </h3>
-
                             <p class={`mt-2 line-clamp-3 text-sm/relaxed ${props.colorMode.text}`}>
                                 {info.description}
                             </p>
