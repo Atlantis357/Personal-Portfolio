df94f0e1dd25f2f0e20f8a13981f4b6b4616deaa
Abraham Alkhatib <aaalkhatib46664@gmail.com>
2023-07-23 18:33:47 -0400

    Added AboutContact

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@
 /*   By: Abraham Alkhatib <aaa26@illinois.edu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:35:16 by Abraham Alk       #+#    #+#             */
-/*   Updated: 2023/07/23 13:42:48 by Abraham Alk      ###   ########.fr       */
+/*   Updated: 2023/07/23 18:33:17 by Abraham Alk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import { foregroundGradients, colorModes } from "./constants";
-import AboutMe from "./components/AboutMe";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import Music from "./components/Music";
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,22 +6,20 @@
 /*   By: Abraham Alkhatib <aaa26@illinois.edu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 21:44:12 by Abraham Alk       #+#    #+#             */
-/*   Updated: 2023/07/23 14:00:02 by Abraham Alk      ###   ########.fr       */
+/*   Updated: 2023/07/23 18:33:15 by Abraham Alk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Wrapper to put Hero and AboutMe in the same view
 import React from 'react'
-import AboutMe from './AboutMe'
 import Hero from './Hero'
-import ContactMe from './ContactMe'
+import AboutContact from './AboutContact.jsx'
 
 const Home = (props) => {
   return (
     <>
         <Hero {...props}/>
-        <AboutMe {...props}/>
-        <ContactMe {...props}/>
+        <AboutContact {...props} />
     </>
   )
 }
